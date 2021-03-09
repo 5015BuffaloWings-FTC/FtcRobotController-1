64cad23985ddfe000f262a22a274af3e996d1014
AikoLozar1 <80004481+AikoLozar1@users.noreply.github.com>
2021-03-09 15:11:17 -0800

    Update Definitions.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Definitions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Definitions.java
@@ -10,10 +10,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Definitions
 {
-    public final int FORWARD = 0;
-    public final int BACKWARD = 1;
-    public final int STRAFELEFT = 2;
-    public final int STRAFERIGHT = 3;
 
     DcMotor leftFrontMotor = null;
     DcMotor leftBackMotor = null;
@@ -22,7 +18,7 @@ public class Definitions
     DcMotor intakeMotor = null;
     DcMotor outputMotor = null;
     Servo outputServo = null;
-    // DcMotor pulleyMotor = null;
+    DcMotor pulleyMotor = null;
     //Servo cLiftServo = null;
     //DigitalChannel bottomLimitSwitch = null;
     //DigitalChannel topLimitSwitch = null;
@@ -37,7 +33,7 @@ public class Definitions
         intakeMotor = Map.dcMotor.get("intakeMotor");
         outputMotor = Map.dcMotor.get("outputMotor");
         outputServo = Map.servo.get("outputServo");
-       // pulleyMotor = Map.dcMotor.get("pulleyMotor");
+        pulleyMotor = Map.dcMotor.get("pulleyMotor");
        // cLiftServo = Map.servo.get("cLiftServo");
        // bottomLimitSwitch = Map.digitalChannel.get("bottomLimitSwitch");
        // topLimitSwitch = Map.digitalChannel.get("topLimitSwitch");
@@ -61,7 +57,7 @@ public class Definitions
         rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //pulleyMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        pulleyMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         outputMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
     }
@@ -72,54 +68,7 @@ public class Definitions
         leftBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //pulleyMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-
-    public int inchesToTicks(double inches)
-    {
-        return (int) ((1120 / (Math.PI * 4)) * inches);
-    }
-
-    //This is used to move a specified number of inches in a gived direction
-    //Sample input robot.moveInches(robot.FORWARD, 12, 0.5);
-    public void moveInches(int direction, double inches, double power)
-    {
-        resetEncoders();//This will make sure all encoders are reset. This is crucial.
-        switch (direction)//This takes the input of the robot direction
-        {
-            case FORWARD: //Forward
-                setDriveForward();
-                moveInches(inches, power);
-                break;
-            case BACKWARD: //Backwards
-                setDriveBackward();
-                moveInches(inches, power);
-                break;
-            case STRAFERIGHT: //Strafe Right
-                setStrafeRight();
-                moveInches(inches, power);
-                break;
-            case STRAFELEFT: //Strafe Left
-                setStrafeLeft();
-                moveInches(inches, power);
-                break;
-            default:
-                break;
-        }
-    }
-
-
-    public void moveInches(double inches, double power)
-    {
-        resetEncoders();
-        leftBackMotor.setTargetPosition(inchesToTicks(inches));
-        leftBackMotor.setPower(power);
-        rightBackMotor.setTargetPosition(inchesToTicks(inches));
-        rightBackMotor.setPower(power);
-        leftFrontMotor.setTargetPosition(inchesToTicks(inches));
-        leftFrontMotor.setPower(power);
-        rightFrontMotor.setTargetPosition(inchesToTicks(inches));
-        rightFrontMotor.setPower(power);
+        
     }
 
     void resetEncoders()
@@ -145,54 +94,5 @@ public class Definitions
         intakeMotor.setPower(power);
 
     }
-
-    void setDriveForward()
-    {
-        leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-    }
-
-    void setDriveBackward()
-    {
-        leftBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
-
-    void setStrafeLeft()
-    {
-        leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD );
-    }
-
-    void setStrafeRight()
-    {
-        leftBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
-
-    void setRotateRight()
-    {
-        leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
-
-    void setRotateLeft()
-    {
-        leftBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-    }
-
 
 }
